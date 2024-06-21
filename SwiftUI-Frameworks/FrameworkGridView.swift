b794cb3cfd0806900d6c0e62ca16207af97c41ca
gelomi26 <gelomi90@gmail.com>
2024-06-22 03:56:12 +0900

    first commit

---
+++ SwiftUI-Frameworks/FrameworkGridView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkGridView.swift
+//  SwiftUI-Frameworks
+//
+//  Created by KyoungSoo Kim on 6/22/24.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkGridView()
+}
