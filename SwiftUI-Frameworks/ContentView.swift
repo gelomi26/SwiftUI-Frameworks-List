f0a781dc487bf99e9a889ee7277ed8cd51c22f81
gelomi26 <gelomi90@gmail.com>
2024-06-22 03:08:03 +0900

    Initial Commit

---
+++ SwiftUI-Frameworks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-Frameworks
+//
+//  Created by KyoungSoo Kim on 6/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-Frameworks/SwiftUI_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FrameworksApp.swift
+//  SwiftUI-Frameworks
+//
+//  Created by KyoungSoo Kim on 6/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
